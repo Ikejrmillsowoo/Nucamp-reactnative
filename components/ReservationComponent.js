0dfe445384c67a75cf15ca1905e6e616a5ccbd58
poppymills2@gmail.com <poppymills2@gmail.com>
2021-04-29 03:33:28 -0400

    Sending Emails

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import * as Animatable from 'react-native-animatable'
+import * as Notifications from 'expo-notifications'
 
 class Reservation extends Component {
   constructor(props) {
@@ -32,7 +33,7 @@ class Reservation extends Component {
                      \nDate: ${this.state.date.toLocaleDateString('en-US')}`
 
     Alert.alert(
-      'Search Reservation',
+      'Search Reservation?',
       message,
       [
         {
@@ -42,7 +43,10 @@ class Reservation extends Component {
         },
         {
           text: 'OK',
-          onPress: () => this.resetForm(),
+          onPress: () => {
+            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'))
+            this.resetForm()
+          },
         },
       ],
       { cancelable: false },
@@ -57,6 +61,31 @@ class Reservation extends Component {
       showCalendar: false,
       // showModal: false,
     })
+  }
+
+  async presentLocalNotification(date) {
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+        }),
+      })
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${date} requested`,
+        },
+        trigger: null,
+      })
+    }
+    let permissions = await Notifications.getPermissionsAsync()
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync()
+    }
+    if (permissions.granted) {
+      sendNotification()
+    }
   }
 
   render() {
@@ -102,7 +131,7 @@ class Reservation extends Component {
               mode={'date'}
               display="default"
               onChange={(event, selectedDate) => {
-                this.setState({ date: selectedDate, ShowCalender: false })
+                selectedDate && this.setState({ date: selectedDate, ShowCalender: false })
               }}
               style={styles.formItem}
             />
